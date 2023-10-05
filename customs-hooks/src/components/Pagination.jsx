7e91017962a4f750dc2c33648450ac8c10085409
=snyder <=>
2023-10-05 16:50:57 -0500

    mejore el componetn ui de la paginacion

--- customs-hooks/src/components/Pagination.jsx
+++ customs-hooks/src/components/Pagination.jsx
@@ -4,26 +4,21 @@ import { useRef } from "react"
 function Pagination() {
     const refElePaginate = useRef()
   return (
-    <div className='mx-auto mt-2 mb-2 w-[300px] border grid grid-cols-3 border-black'>
-        <div className='flex justify-center items-center overflow-x-hidden'>"menos</div>
-        <div ref={refElePaginate} className='border justify-center flex items-center py-1 overflow-auto '>
-            <li className='m-1 w-[50px] h-[40px] border border-gray-400 bg-gray-400'></li>
-            <li className='m-1 w-[50px] h-[40px] border border-gray-400 bg-gray-400'></li>
-            <li className='m-1 w-[50px] h-[40px] border border-gray-400 bg-gray-400'></li>
-            <li className='m-1 w-[50px] h-[40px] border border-gray-400 bg-gray-400'></li>
-            <li className='m-1 w-[50px] h-[40px] border border-gray-400 bg-gray-400'></li>
+    <div className='mx-auto mt-2 mb-2 w-[350px] h-[50px] border grid grid-cols-3 rounded border-black list-none'>
+        <button onClick={()=>refElePaginate.current.scrollLeft -= 58} className="flex items-center  justify-center">"menos"</button>
+        <div ref={refElePaginate} className='border flex items-center      overflow-hidden '>
+
+           
+            <button className="m-1 rounded flex-shrink-0 inline-block border w-[50px] h-[45px]">1</button>
+            <button className="m-1 rounded flex-shrink-0 inline-block border w-[50px] h-[45px]">2</button>
+            <button className="m-1 rounded flex-shrink-0 inline-block border w-[50px] h-[45px]">3</button>
+            <button className="m-1 rounded flex-shrink-0 inline-block border w-[50px] h-[45px]">4</button>
+            <button className="m-1 rounded flex-shrink-0 inline-block border w-[50px] h-[45px]">5</button>
+             
+            
             
         </div>
-        <div onClick={()=>{
-            let scrollWidth = refElePaginate.current.scrollWidth
-            console.log("ancho del scroll horizontal", scrollWidth)
-            
-            console.log("ancho del scroll horizontal", scrollWidth)
-            // propiedad para mover el scroll x para ir avanzando poco a poco
-            console.log("scroll left" ,refElePaginate.current.scrollLeft)
-            const scrollLeft = refElePaginate.current.scrollLeft += 10
-            
-        }} className='flex justify-center items-center'>"mas"</div>
+        <button onClick={()=>refElePaginate.current.scrollLeft += 58 } className="flex items-center  justify-center">"mas"</button>
     </div>
   )
 }
